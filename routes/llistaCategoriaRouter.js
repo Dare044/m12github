b489f764dfb29656e0958859ca841e3ebc8dbb11
ericlogut <ericlogut@gmail.com>
2022-11-03 09:42:03 +0000

    3 noviembre

--- routes/llistaCategoriaRouter.js
+++ routes/llistaCategoriaRouter.js
@@ -8,14 +8,14 @@ const llistaCategoria_controller = require("../controllers/llistaCategoriaContro
 router.get("/", llistaCategoria_controller.list);
 
 
-// router.get("/create", personal_controller.create_get);
-// router.post("/create", personal_controller.create_post);
+router.get("/create", llistaCategoria_controller.create_get);
+router.post("/create", llistaCategoria_controller.create_post);
 
-// router.get("/delete/:id", personal_controller.delete_get);
-// router.post("/delete/:id", personal_controller.delete_post);
+router.get("/delete/:id", llistaCategoria_controller.delete_get);
+router.post("/delete/:id", llistaCategoria_controller.delete_post);
 
-// router.get("/update/:id", personal_controller.update_get);
-// router.post("/update/:id", personal_controller.update_post);
+// router.get("/update/:id", llistaCategoria_controller.update_get);
+// router.post("/update/:id", llistaCategoria_controller.update_post);
 
 
 module.exports = router;
