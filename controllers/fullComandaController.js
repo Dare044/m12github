6a99f63869b0dd67e2b32ba4388c861f046622c1
ericlogut <ericlogut@gmail.com>
2022-12-15 10:31:00 +0100

    ShowFullComanda

--- controllers/fullComandaController.js
+++ controllers/fullComandaController.js
@@ -1,5 +1,9 @@
 var FullComanda = require("../models/fullComanda");
 var LlistatProveidor = require("../models/llistatProveidor");
+var PropostaPressupost = require("../models/propostaPressupost");
+var PropostaNecessitat = require("../models/propostaNecessitat");
+var LlistaCategoria = require("../models/llistaCategoria");
+
 class FullComandaController {
 
   // Version 1
@@ -7,7 +11,11 @@ class FullComandaController {
     try {
       var list_fullComandes = await FullComanda.find();
       var list_ProveidorsLlista = await LlistatProveidor.find();
-      res.render('fullComandes/list',{list:list_fullComandes, list_ProveidorsLlista:list_ProveidorsLlista});   
+      var list_LlistaCategoria = await LlistaCategoria.find();
+      res.render('fullComandes/list',{list:list_fullComandes,
+                                      list_LlistaCategoria:list_LlistaCategoria, 
+                                      list_ProveidorsLlista:list_ProveidorsLlista, 
+                                      list_fullComandes:list_fullComandes});   
     }
     catch(e) {
       res.send('Error!');
@@ -71,7 +79,7 @@ class FullComandaController {
 //   }
 
   static async delete_get(req, res, next) {
-     res.render('fullComandes/delete',{id: req.params.id})
+    res.render('fullComandes/delete',{id: req.params.id})
   }
 
   static async delete_post(req, res, next) {
@@ -86,22 +94,25 @@ class FullComandaController {
   }
 
   static async show_get(req, res, next) {
-    res.render('fullComandes/show',{id: req.params.id,})
-    
+    var list_propostaNecessitat = await PropostaNecessitat.find();
+    var list_propostaPressupost = await PropostaPressupost.find();
+    var tipusProposta = "";
+    list_propostaNecessitat.forEach(function(propostaNecessitat) {
+      if (propostaNecessitat.idFullComanda == req.params.id) {
+        tipusProposta = "PropostaDeNecessitat";
+      } else {
+        tipusProposta = "PropostaDePressupost";
+      }
+    });
+
+    var list_LlistaCategoria = await LlistaCategoria.find();
+    res.render('fullComandes/show',{id: req.params.id, 
+                                    list_propostaNecessitat:list_propostaNecessitat, 
+                                    list_propostaPressupost:list_propostaPressupost , 
+                                    list_LlistaCategoria:list_LlistaCategoria,
+                                    tipusProposta:tipusProposta})
     
  }
-
- static async show_post(req, res, next) {
-   
-   FullComanda.findByIdAndRemove(req.params.id, function (error) {
-     if(error){
-       res.redirect('/fullComanda')
-     }else{
-       res.redirect('/fullComanda')
-     }
-   }) 
- }
-
 }
 
 module.exports = FullComandaController;
